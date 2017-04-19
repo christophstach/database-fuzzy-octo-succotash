adb3322e55d8c6f8c23c3e36eda73d8c51031176
SaizFerri <adri_esp_11@hotmail.com>
2017-04-20 01:44:26 +0200

    Added insert into flats

---
+++ src/main/resources/insert-data.sql
@@ -0,0 +1,7 @@
+/**Insert into flats*/
+INSERT INTO flats (street, postal_code, city) VALUES ("Glanzstr.", "12437", "Berlin");
+INSERT INTO flats (street, postal_code, city) VALUES ("Plönzeile", "12459", "Berlin");
+INSERT INTO flats (street, postal_code, city) VALUES ("Leopoldo Alas", "22345", "Oviedo");
+INSERT INTO flats (street, postal_code, city) VALUES ("Simon-Dach-Str.", "12457", "Berlin");
+INSERT INTO flats (street, postal_code, city) VALUES ("Mühlenstr.", "33880", "Hamburg");
+INSERT INTO flats (street, postal_code, city) VALUES ("Nanclares de Oca", "28022", "Madrid");
