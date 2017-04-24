0f27cf37d32649244d0f0764bf3c179375b543b3
SaizFerri <adri_esp_11@hotmail.com>
2017-04-24 20:20:32 +0200

    Finished all sql queries

--- src/main/resources/select.sql
+++ src/main/resources/select.sql
@@ -24,3 +24,64 @@ SELECT first_name FROM persons p3 ,
     )
   ) AS tmp
 WHERE tmp.person_id = p3.person_id;
+
+/*Wer kauft was? (Kartetisches produkt)*/
+SELECT pro.name, p.first_name, p.last_name, c.name
+FROM persons p, persons_buy_products buy, products pro, categories c, products_have_categories cp
+WHERE buy.person_id = p.person_id AND buy.product_id = pro.product_id AND c.category_id = cp.category_id AND cp.product_id = pro.product_id;
+/*Wer kauft was? (JOIN)*/
+SELECT pro.name, p.first_name, p.last_name, c.name FROM persons p
+JOIN persons_buy_products buy ON p.person_id = buy.person_id
+JOIN products pro ON buy.product_id = pro.product_id
+JOIN products_have_categories cp ON pro.product_id = cp.product_id
+JOIN categories c ON cp.category_id = c.category_id;
+
+/*Alle Leute die keine zuständigkeit für ein Raum in Berlin haben(NOT IN)*/
+SELECT p.first_name, p.last_name
+FROM persons p
+WHERE p.person_id NOT IN
+  (
+    SELECT DISTINCT pr.person_id FROM flats f
+    JOIN rooms r ON f.flat_id = r.flat_id
+    JOIN persons_are_responsible_for_rooms pr ON r.room_id = pr.room_id
+    WHERE f.city = 'Berlin'
+  );
+/*Welche Wohnungen sind nicht leer* (IN)*/
+SELECT * FROM flats f
+WHERE f.flat_id IN
+  (
+    SELECT p.flat_id
+    FROM persons p
+  );
+
+/*Summe aller Punkte die ein Raum geben (sum())*/
+SELECT f.street, r.name, sum(t.points) FROM rooms r
+JOIN flats f ON r.flat_id = f.flat_id
+JOIN tasks t ON r.room_id = t.room_id
+GROUP BY f.flat_id, r.room_id;
+/*Anzahl der Zimmer pro Wohnung (count())*/
+SELECT f.street, f.postal_code, f.city, count(*) FROM rooms r
+JOIN flats f ON r.flat_id = f.flat_id
+GROUP BY f.flat_id;
+/*Durchschnittpreis einer Kategorie (avg())*/
+SELECT c.name, avg(p.price) FROM products p
+JOIN products_have_categories pc ON p.product_id = pc.product_id
+JOIN categories c ON pc.category_id = c.category_id
+GROUP BY c.category_id;
+/*Teursten Preis in jede Kategorie (max())*/
+SELECT c.name, max(p.price) FROM products p
+JOIN products_have_categories pc ON p.product_id = pc.product_id
+JOIN categories c ON pc.category_id = c.category_id
+GROUP BY c.category_id;
+
+/*Wohnungen wo mehr als 7 aufgaben insgesammt gibt*/
+SELECT f.flat_id, f.street, f.city, count(*) FROM flats f
+JOIN rooms r ON f.flat_id = r.flat_id
+JOIN tasks t ON r.room_id = t.room_id
+GROUP BY f.flat_id
+HAVING count(*) > 7;
+/*Leute die mehr als 15 euro ausgegeben haben*/
+SELECT p.first_name, p.last_name, sum(pr.price * pb.quantity) FROM persons p
+JOIN persons_buy_products pb ON p.person_id = pb.person_id
+JOIN products pr ON pb.product_id = pr.product_id
+GROUP BY p.person_id HAVING sum(pr.price * pb.quantity) > 15;
